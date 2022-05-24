5f00293948eabc0a3529d47bf216068da1d61a60
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-05-24 14:26:40 -0300

    Aula 121. Gravando os dados do formulário no banco de dados

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     // site_contatos // padrão do framework
-
     
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
+
+
 }

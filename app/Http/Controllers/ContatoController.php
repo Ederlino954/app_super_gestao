d704ff01eeaadb560226a5720743c791b2650eb6
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-05-24 19:09:02 -0300

    Aula 127 Refactoring do projeto Super Gestão - parte 2

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\MotivoContato;
 use Illuminate\Http\Request;
 use App\SiteContato;
 
@@ -9,11 +10,7 @@ class ContatoController extends Controller
 {
     public function contato(Request $request){
 
-        $motivo_contatos = [
-            '1' => 'Dúvidas',
-            '2' => 'Elogio',
-            '3' => 'Reclamação',
-        ];
+        $motivo_contatos = MotivoContato::all();
 
         return view('site.contato',  ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }

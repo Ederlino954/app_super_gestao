623568d09754ea7e25eecb786d21139f66e00c29
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-05-25 02:12:27 -0300

    Aula 136. Criando  middleware LogAcessoMiddleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,12 @@ use Illuminate\Support\Facades\Route;
 //     return 'Hello World';
 // });
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+// Route::get('/', 'PrincipalController@principal')->name('site.index')->middleware('LogAcessoMiddleware');
+
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/', 'PrincipalController@principal')
+    ->name('site.index'); // outra padrão de uso de middleware
+
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
